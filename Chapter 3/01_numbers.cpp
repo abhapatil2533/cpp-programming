9f3db8205515228f493e7ef187d68964047007ab
abhapatil2533 <abha.a.patil@gmail.com>
2024-09-26 18:57:00 +0530

    Add files via upload

---
+++ Chapter 3/01_numbers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num=5;
+    double numm=5.5;
+    num++;      //increments
+    num +=80;  //adds 80, same for *-/+
+    cout<<num;
+    //cout<<4+5*10;     //this will perform 5*10 first
+    
+    return 0;
+}
---
+++ Chapter 3/02.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+
+    cout<<pow(2,5);  //raised to 
+    cout<<sqrt(36);
+    cout<<round(4.4);
+    cout<<fmax(3,10);
+}

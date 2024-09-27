e58a8d47cea9e9bca38500455a40a2607cf8fb70
abhapatil2533 <abha.a.patil@gmail.com>
2024-09-27 23:54:45 +0530

    Add files via upload

---
+++ Chapter 4/add.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n1,n2;
+    cout<<"Enter a number 1";
+    cin>>n1;
+    cout<<"Enter a number 2";
+    cin>>n2;
+    cout<<n1+n2;
+    return 0;
+}
---
+++ Chapter 4/getting_input.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cout<<"Enter your age";
+    cin>>age;            //this is for int double and char
+    cout<<"you are"<<age<<"years old";
+    return 0;
+}

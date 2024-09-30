67eb1395baba180440ef16447ecc254d7c1c9dbc
abhapatil2533 <abha.a.patil@gmail.com>
2024-09-30 19:16:10 +0530

    Add files via upload

---
+++ Chapter 6/if_stmts.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    bool isMale= true;
+    bool istall= true;
+    if(isMale && istall)             // ||for or, && is and, logic gates rules work
+    cout<<"You are a tall male";
+    else
+    cout<<"You are not male";
+    return 0;
+}

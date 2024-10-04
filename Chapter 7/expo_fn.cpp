77ad0624f25b2c58e66438402a56ac8a09c50112
abhapatil2533 <abha.a.patil@gmail.com>
2024-10-04 14:22:45 +0530

    Add files via upload

---
+++ Chapter 7/expo_fn.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int power(int basenum,int pownum)
+{
+    int result =1;
+    for(int i=0;i<pownum;i++)
+    {
+        result=result*basenum;
+    }
+    return result;
+}
+int main()
+{
+    cout<<power(8,2);
+    return 0;
+}
---
+++ Chapter 7/forrloop.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+// {
+//     int i;
+//     for(i=1; i<=5; i++)
+//     {
+//         cout<<i<<endl;
+//     }
+//     return 0;
+// }
+
+{
+    int nums[]={7,6,4,3,2,9,9};
+    for(int i=0;i<=5;i++)
+    {
+        cout<<nums[i]<<endl;
+    }
+    return 0;
+}
